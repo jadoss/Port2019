e6c10ed6a9fdbcb230185deee70b2d0a23bc6a4f
x <x@xs-macbook-pro.attlocal.net>
2019-04-23 11:31:27 -0500

    Updates

---
+++ precache-manifest.a27380ba3196d049ab4f9ebd88c8dc19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f4f34e518d3ae7ce2e4d79d0993c70",
+    "url": "/Port2019/index.html"
+  },
+  {
+    "revision": "a4625360bedab9dbd2ec",
+    "url": "/Port2019/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "a05aa06903fb77a36258",
+    "url": "/Port2019/static/css/main.bb00302f.chunk.css"
+  },
+  {
+    "revision": "a4625360bedab9dbd2ec",
+    "url": "/Port2019/static/js/2.1a008c79.chunk.js"
+  },
+  {
+    "revision": "a05aa06903fb77a36258",
+    "url": "/Port2019/static/js/main.24538b14.chunk.js"
+  },
+  {
+    "revision": "52466987223715ea8b51",
+    "url": "/Port2019/static/js/runtime~main.f84e61fa.js"
+  }
+]);
